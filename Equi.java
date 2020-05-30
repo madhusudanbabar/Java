7170731dff83691cb10e6f662791b4cec4b5d54d
Komal Bapurao Supekar <55024646+komalsupekar@users.noreply.github.com>
2020-05-30 12:28:14 +0530

    Add files via upload

---
+++ Equi.java
@@ -0,0 +1,30 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+class Equi {
+    public static void main(String args[] ) throws Exception {
+        
+        Scanner s = new Scanner(System.in);
+        int a=s.nextInt();
+        int n=s.nextInt();
+        //while(n!=0){
+        if(a%3==0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        //}n--;
+    }
+
+
+    }
+} 
